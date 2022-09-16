f8039eefdd4a92be05b85583cdf84a50fe722d9e
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-16 22:18:20 +0300

    выравнил код

--- Program.cs
+++ Program.cs
@@ -4,18 +4,18 @@ Console.WriteLine("Введите число: ");
 string input = Console.ReadLine()!;
 int number = int.Parse(input);
 
-if(number <= 0)
+if (number <= 0)
 {
     Console.WriteLine("введите положительное число");
 }
 
 int count = 1;
-while(count <= number)
+while (count <= number)
 {
-    if(count % 2 == 0)
+    if (count % 2 == 0)
     {
         Console.Write(count);
         Console.Write(", ");
     }
-    count++;
+    count ++;
 }
