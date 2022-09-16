f6141414e9a42fa5db11a9641e5b1b88be8ab538
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-16 16:16:21 +0300

    task8

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string input = Console.ReadLine()!;
+int number = int.Parse(input);
+
+if(number <= 0)
+{
+    Console.WriteLine("введите положительное число");
+}
+
+int count = 1;
+while(count <= number)
+{
+    if(count % 2 == 0)
+    {
+        Console.Write(count);
+        Console.Write(", ");
+    }
+    count++;
+}
