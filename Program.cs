83175cae771471f3216bb0805ebfaeebc873d6be
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-19 19:55:06 +0300

    оптимизировал код

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ if (number <= 0)
     Console.WriteLine("введите положительное число");
 }
 
-int count = 1;
+int count = 2;
 while (count <= number)
 {
     if (count % 2 == 0)
@@ -17,5 +17,5 @@ while (count <= number)
         Console.Write(count);
         Console.Write(", ");
     }
-    count ++;
+    count +2;
 }
