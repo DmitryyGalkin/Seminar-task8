6b938e09246710ca75b1a7c89041018c65027098
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-20 13:17:31 +0300

    Оптимизация кода

--- Program.cs
+++ Program.cs
@@ -12,10 +12,9 @@ if (number <= 0)
 int count = 2;
 while (count <= number)
 {
-    if (count % 2 == 0)
+    
     {
-        Console.Write(count);
-        Console.Write(", ");
+        Console.Write($"{count} ");
     }
-    count +2;
+    count +=2;
 }
